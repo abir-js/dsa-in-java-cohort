e497219f9d8d19a9d248b6c771e20d7592573b96
abir <abir.bhattacharjee2580@gmail.com>
2025-08-02 01:58:26 +0530

    day-1: ArrayList and LinkedList code

--- day_1/Collections.java
+++ day_1/Collections.java
@@ -1,20 +1,34 @@
-// package day_1;
 import java.util.*;
 
 public class Collections {
+
     public static void main(String[] args) {
-        
         List<Integer> a = new ArrayList<>();
+        //* Here list is interface and ArrayList is class
+        LinkedList<Integer> b = new LinkedList<>();
+        //* Here we are using both LinkedList interface and class to use exclusive methods like addFirst, removeFirst.
+        
+
+        System.out.println(a.size());
         a.add(10);
+        System.out.println(a.size());
         a.add(20);
+        System.out.println(a.size());
         a.add(30);
-        a.add(2, 50);
+        System.out.println(a.size());
+        // b.addFirst(50); // exclusive method of LinkedList
+        // b.removeFirst(); // exclusive method of LinkedList
 
-        a.remove(2);
+        // a.remove(2);
 
-        for(int i = 0; i< a.size(); i++) {
-            System.out.print(a.get(i) + ", ");
-        }
+        System.out.println(a);
+        // for(int i = 0; i < a.size(); i++){
+        //     System.out.println(a.get(i));
+        // }
+        // Iterator<Integer> it = a.iterator();
+        // while (it.hasNext()) {
+        //     System.out.println(it.next());
+        // }
+
     }
 }
-
