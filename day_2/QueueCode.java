d379bf8a7e2895f94ccfff752633d2d2fb2163f8
abir <abir.bhattacharjee2580@gmail.com>
2025-08-02 23:48:26 +0530

    QueueCode.java

--- day_2/QueueCode.java
+++ day_2/QueueCode.java
@@ -6,10 +6,12 @@ public class QueueCode {
     public static void main(String[] args) {
         Queue<Integer> q = new LinkedList<>();
 
-        //* offer() => adds elements withot warning
-        q.offer(10);        
-        q.offer(20);        
-        q.offer(30);    
+        // * offer() => adds elements withot warning
+        q.offer(10);
+        q.offer(20);
+        q.offer(30);
+        q.offer(50);
+        q.offer(40);
 
         /*
          * poll() -> removes and returns head of Queue
@@ -17,10 +19,10 @@ public class QueueCode {
          */
 
         System.out.println(q);
-        System.out.println("poll: "+ q.poll());
+        System.out.println("poll: " + q.poll());
         System.out.println("after polling, value of queue: ");
         System.out.println(q);
-        System.out.println("peek: "+ q.peek());
+        System.out.println("peek: " + q.peek());
 
     }
 }
