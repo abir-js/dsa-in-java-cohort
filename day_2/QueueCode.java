087d08575300660f59e6d0b21243469a7beeed68
abir <abir.bhattacharjee2580@gmail.com>
2025-08-02 18:30:20 +0530

    queue operations

---
+++ day_2/QueueCode.java
@@ -0,0 +1,26 @@
+package day_2;
+
+import java.util.*;
+
+public class QueueCode {
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+
+        //* offer() => adds elements withot warning
+        q.offer(10);        
+        q.offer(20);        
+        q.offer(30);    
+
+        /*
+         * poll() -> removes and returns head of Queue
+         * peek() => gets head of Queue
+         */
+
+        System.out.println(q);
+        System.out.println("poll: "+ q.poll());
+        System.out.println("after polling, value of queue: ");
+        System.out.println(q);
+        System.out.println("peek: "+ q.peek());
+
+    }
+}
