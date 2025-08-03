256f249f00c0ed3257cec80b8465911afd98845f
abir <abir.bhattacharjee2580@gmail.com>
2025-08-03 22:18:43 +0530

    Refactor comments for clarity and correct spelling in HashMap example

--- day_2/HashMapCode.java
+++ day_2/HashMapCode.java
@@ -13,25 +13,26 @@ public class HashMapCode {
         student.put(4, "Projit");
         student.put(5, "Gopal");
 
-        // ? If you put 2 same keys then previous element gets override
+        // * If you put 2 same keys then previous element gets override
 
         if (!student.containsKey(5)) {
             student.put(5, "Ram");
         }
-        student.putIfAbsent(5, "Somya");
+        student.putIfAbsent(5, "Soumya");
 
         System.out.println(student);
 
-        // ? Access only keys from map
+        // * Access only keys from map
         for (Integer rollNo : student.keySet()) {
             System.out.println(rollNo);
         }
 
-        //? Access only values
+        //* Access only values
         for (String name : student.values()) {
             System.out.println(name);
         }
 
+        //* */ Access keys and values
         for(Map.Entry<Integer, String> me: student.entrySet()) {
             System.out.println(me.getKey());
             System.out.println(me.getValue());
