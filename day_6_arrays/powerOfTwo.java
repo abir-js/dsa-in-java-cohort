2e0513d68dc834693160160b4dbe53434de8d140
Abir Bhattacharjee <abir.bhattacharjee2580@gmail.com>
2025-08-21 08:43:18 +0530

    powerOf2

---
+++ day_6_arrays/powerOfTwo.java
@@ -0,0 +1,14 @@
+package day_6_arrays;
+
+public class powerOfTwo {
+
+    public boolean isPowerOfTwo(double n) {
+        if (n == 1)
+            return true;
+        else if (n < 1)
+            return false;
+        else
+            return isPowerOfTwo(n / 2.0);
+    }
+
+}
